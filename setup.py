9881e3f59b5c1f111b11828db7697f979a850436
FelixTheC <felixeisenmenger@gmx.net>
2020-06-26 11:34:07 +0200

    [CRE] basic setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@created: 30.04.20
+@author: felix
+"""
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+setup(
+    name="flask_bp_autoregister",
+    version="0.0.1",
+    description="Auto register all project flask-blueprints.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FelixTheC/flask_bp_autoregister",
+    author="FelixTheC",
+    author_email="fberndt87@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=['flask_bp_autoregister'],
+    python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=[
+        "flask>=1.1.0"
+    ]
+)
