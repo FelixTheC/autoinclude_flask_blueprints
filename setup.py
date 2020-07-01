0b49993ea10ee4aba7899ed05c5dabf5d225fb77
FelixTheC <felixeisenmenger@gmx.net>
2020-07-01 20:50:02 +0200

    [UPD] version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 setup(
     name="autoinclude_blueprints",
-    version="1.0.0",
+    version="1.0.1",
     description="Auto register all project flask-blueprints.",
     long_description=README,
     long_description_content_type="text/markdown",
